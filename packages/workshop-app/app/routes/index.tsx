9be7843f6a33fa20fd6a6caaf202f8df8d2f92c8
Ayush <62958067+Ayush-v@users.noreply.github.com>
2023-03-18 14:20:08 +0530

    index page css improved

--- packages/workshop-app/app/routes/index.tsx
+++ packages/workshop-app/app/routes/index.tsx
@@ -24,21 +24,20 @@ export async function loader() {
 
 export default function Index() {
 	const data = useLoaderData<typeof loader>()
+
 	return (
-		<main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
+		<main className="flex min-h-screen items-center justify-center">
 			<div>
-				<h1 className="mb-7 text-4xl font-bold">{data.title}</h1>
+				<h1 className="mb-10 text-4xl font-extrabold">{data.title}</h1>
 				<div>
 					<ul className="flex flex-col gap-2">
 						{data.exercises.map(exercise => (
 							<li key={exercise.exerciseNumber}>
 								<Link
 									to={`${exercise.exerciseNumber.toString().padStart(2, '0')}`}
-									className="clip-path-button mr-auto inline-flex min-w-fit max-w-xs border-2 border-black bg-black px-8 py-4 font-bold text-white outline-none hover:bg-white hover:text-black focus:bg-white focus:text-black"
+									className="clip-path-button inline-flex border-2 border-black bg-black px-8 py-4 font-bold text-white outline-none hover:bg-white hover:text-black focus:bg-white focus:text-black"
 								>
-									<div className="flex flex-wrap">
-										{exercise.exerciseNumber}. {exercise.title}
-									</div>
+									{exercise.exerciseNumber}. {exercise.title}
 								</Link>
 							</li>
 						))}
