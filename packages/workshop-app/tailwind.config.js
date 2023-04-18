a59ebff647f803a24b12574c31678c0367448949
onemen <tabmix.onemen@gmail.com>
2023-04-18 18:14:59 +0300

    fix: remove prose margin from list item first/last child (#37)

--- packages/workshop-app/tailwind.config.js
+++ packages/workshop-app/tailwind.config.js
@@ -1,6 +1,21 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
+const removeProseMargin = {
+	'> ul > li > *:first-child': {
+		marginTop: 'unset',
+	},
+	'> ul > li > *:last-child': {
+		marginBottom: 'unset',
+	},
+	'> ol > li > *:first-child': {
+		marginTop: 'unset',
+	},
+	'> ol > li > *:last-child': {
+		marginBottom: 'unset',
+	},
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./app/**/*.{ts,tsx,jsx,js}', '../example/**/*.md'],
@@ -17,6 +32,14 @@ module.exports = {
 			},
 			colors: {
 				gray: colors.neutral,
+			},
+			typography: {
+				DEFAULT: { css: [removeProseMargin] },
+				sm: { css: [removeProseMargin] },
+				base: { css: [removeProseMargin] },
+				lg: { css: [removeProseMargin] },
+				xl: { css: [removeProseMargin] },
+				'2xl': { css: [removeProseMargin] },
 			},
 		},
 	},
