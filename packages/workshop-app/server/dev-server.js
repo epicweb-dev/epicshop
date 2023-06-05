dbb9fdf9852e1d4d592a113add4888b3944792b8
onemen <tabmix.onemen@gmail.com>
2023-06-05 17:11:41 +0300

    start tsx watch as child process (#66)
    * start tsx watch as child process
    
    * dev-server execa command fails on Mac & Linux

---
+++ packages/workshop-app/server/dev-server.js
@@ -0,0 +1,18 @@
+import { execa } from 'execa'
+
+if (process.env.NODE_ENV === 'production') {
+	await import('./start.js')
+} else {
+	const command =
+		'tsx watch --clear-screen=false --ignore "app/**" --ignore "build/**" --ignore "node_modules/**" --inspect ./start.js'
+	execa(command, {
+		stdio: ['ignore', 'inherit', 'inherit'],
+		shell: true,
+		env: {
+			...process.env,
+			FORCE_COLOR: true,
+		},
+		// https://github.com/sindresorhus/execa/issues/433
+		windowsHide: false,
+	})
+}
