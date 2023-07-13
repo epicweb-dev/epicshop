e88bbb7c18dbcc69b5a51c805805f91daa8a7798
Kent C. Dodds <me+github@kentcdodds.com>
2023-07-13 16:58:49 -0600

    fix(client-hints): ensure path is always set properly

--- packages/workshop-app/app/utils/client-hints.tsx
+++ packages/workshop-app/app/utils/client-hints.tsx
@@ -114,7 +114,7 @@ ${Object.values(clientHints)
 for (const hint of hints) {
 	if (hint.cookie !== hint.actual) {
 		cookieChanged = true;
-		document.cookie = hint.name + '=' + hint.actual;
+		document.cookie = encodeURIComponent(hint.name) + '=' + encodeURIComponent(hint.actual) + ';path=/';
 	}
 }
 if (cookieChanged) {
