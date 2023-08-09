5dbbfab2af37c0f98270ca9401699542fd949ae8
Kent C. Dodds <me+github@kentcdodds.com>
2023-08-09 10:13:09 -0600

    fix: add default NODE_ENV to bundleMDX

--- packages/workshop-app/utils/compile-mdx.server.ts
+++ packages/workshop-app/utils/compile-mdx.server.ts
@@ -152,6 +152,15 @@ export async function compileMdx(
 		const bundleResult = await queuedBundleMDX({
 			file,
 			cwd: path.dirname(file),
+			esbuildOptions(options) {
+				options.define = {
+					'process.env.NODE_ENV': JSON.stringify(
+						process.env.NODE_ENV ?? 'production',
+					),
+					...options.define,
+				}
+				return options
+			},
 			mdxOptions(options) {
 				options.remarkPlugins = [
 					...(options.remarkPlugins ?? []),
@@ -214,6 +223,15 @@ export async function compileMarkdownString(markdownString: string) {
 			try {
 				const result = await queuedBundleMDX({
 					source: markdownString,
+					esbuildOptions(options) {
+						options.define = {
+							'process.env.NODE_ENV': JSON.stringify(
+								process.env.NODE_ENV ?? 'production',
+							),
+							...options.define,
+						}
+						return options
+					},
 					mdxOptions(options) {
 						options.rehypePlugins = [
 							...(options.rehypePlugins ?? []),
