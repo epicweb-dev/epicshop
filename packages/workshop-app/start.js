35bba3562751a7036372b29be97679e169e35606
Kent C. Dodds <me+github@kentcdodds.com>
2023-08-09 12:12:30 -0600

    feat: add unhandledRejection handler

--- packages/workshop-app/start.js
+++ packages/workshop-app/start.js
@@ -40,3 +40,8 @@ if (process.env.NODE_ENV === 'production') {
 		process.exit(1)
 	})
 }
+
+process.on('unhandledRejection', (reason, promise) => {
+	console.error('Unhandled Rejection at:', promise, 'reason:', reason)
+	throw reason // This will trigger an 'uncaughtException' event
+})
