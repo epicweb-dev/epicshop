355884c2c47490f86f6ee06b05da0adcb1d9ac91
Kent C. Dodds <me+github@kentcdodds.com>
2024-05-24 16:12:45 -0600

    chore(prettier): use @epic-web/config

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+import defaultConfig from '@epic-web/config/prettier'
+
+/** @type {import("prettier").Options} */
+export default {
+	...defaultConfig,
+	tailwindConfig: './packages/workshop-app/tailwind.config.ts',
+}
