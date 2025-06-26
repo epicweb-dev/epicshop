903d30a5b19bf359adc473db28c4ccd47e5381b3
Kent C. Dodds <me+github@kentcdodds.com>
2025-06-26 15:32:41 -0600

    chore: format

--- packages/workshop-app/app/routes/_app+/l.tsx
+++ packages/workshop-app/app/routes/_app+/l.tsx
@@ -6,14 +6,17 @@ export async function loader({ request }: LoaderFunctionArgs) {
 	if (!exercises.length) {
 		throw new Response('No exercises found', { status: 404 })
 	}
+
 	const lastExercise = exercises[exercises.length - 1]
 	if (!lastExercise || !lastExercise.steps || !lastExercise.steps.length) {
 		throw new Response('No steps found in last exercise', { status: 404 })
 	}
+
 	const lastStep = lastExercise.steps[lastExercise.steps.length - 1]
 	if (!lastStep || !lastStep.solution) {
 		throw new Response('No solution found for last step', { status: 404 })
 	}
+
 	const exerciseNumber = lastExercise.exerciseNumber.toString().padStart(2, '0')
 	const stepNumber = lastStep.stepNumber.toString().padStart(2, '0')
 	return redirect(`/exercise/${exerciseNumber}/${stepNumber}/solution`)
