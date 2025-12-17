058ab9bab0703e576fadbcbaf9b2add680a843ad
Kent C. Dodds <me+github@kentcdodds.com>
2025-12-17 15:18:43 -0700

    chore: update import paths in release.ts to use .js extensions
    - Changed import statements in release.ts to use '.js' extensions for consistency with recent updates in the project structure.

--- other/release.ts
+++ other/release.ts
@@ -1,8 +1,8 @@
 import path from 'path'
 import { createProjectGraphAsync, workspaceRoot } from '@nx/devkit'
 import fs from 'fs-extra'
-import { releaseChangelog, releasePublish, releaseVersion } from 'nx/release'
-import { findMatchingProjects } from 'nx/src/utils/find-matching-projects.ts'
+import { releaseChangelog, releasePublish, releaseVersion } from 'nx/release/index.js'
+import { findMatchingProjects } from 'nx/src/utils/find-matching-projects.js'
 import { default as yargs } from 'yargs'
 
 async function go() {
